56079d448a03342e588b8c9b602b32c97ef986bc
Felix Bade <flix.bade@gmail.com>
2019-07-23 22:30:11 +0300

    Update oletkofuksibot.py

--- oletkofuksibot.py
+++ oletkofuksibot.py
@@ -1,7 +1,7 @@
 import time
 import requests
 
-from secret import tg_token
+from secret import tg_token, from_chat_id, message_id
 
 def getUpdates(offset):
     url = 'https://api.telegram.org/bot%s/getUpdates?offset=%d' % (tg_token, offset)
@@ -11,6 +11,11 @@ def getUpdates(offset):
 def sendMessage(chat_id, text):
     url = 'https://api.telegram.org/bot%s/sendMessage' % tg_token
     data = {'chat_id': chat_id, 'text': text}
+    requests.post(url, data)
+
+def forwardMessage(to_chat_id, from_chat_id, message_id):
+    url = 'https://api.telegram.org/bot%s/forwardMessage' % tg_token
+    data = {'chat_id': to_chat_id, 'from_chat_id': from_chat_id, 'message_id': message_id}
     requests.post(url, data)
 
 lastUpdateId = 0
@@ -24,9 +29,9 @@ while True:
 
         for new_member in new_members:
             if new_member.get('is_bot') == True:
-                sendMessage(chat_id, 'Fuksibotti?')
+                forwardMessage(chat_id, from_chat_id, message_id)
             else:
-                sendMessage(chat_id, 'Fuksi?')
+                forwardMessage(chat_id, from_chat_id, message_id)
 
     print (updates)
     time.sleep(1)
